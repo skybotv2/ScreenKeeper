dc69517ddc74f9594f30bfb473cf378eb8f496de
skybotv2 <86743132+skybotv2@users.noreply.github.com>
2024-08-10 11:29:01 -0500

    SC | update

--- src/main/java/main/java/ScreenKeeper.java
+++ src/main/java/main/java/ScreenKeeper.java
@@ -1,7 +1,8 @@
 package main.java;
 
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -9,40 +10,48 @@ public class ScreenKeeper {
     private static final Log log = LogFactory.getLog(ScreenKeeper.class);
 
     public static void main(String[] args) {
-         try {
-          
-            createScreen(args);
-           
-        } catch (Exception e) {
-            log.error("Fatal error will terminate: " + e);
+        while(true){
+            System.out.println("Hello World");
+            System.out.flush();
+    
+            
+                try {
+                    createScreen(args);
+                } catch (Exception e) {
+                    log.error("Fatal error will terminate:", e);
+                    log.error(e.getStackTrace(), e);
+                }
         }
+        
+        
     } 
 
-    private static void createScreen(String[] args) {
-        String timer = parseArguments(args,0);
-        log.info("Timer: " + timer);
-        if (timer.length() <= 0 ){
-            log.info("Please enter how long to keep the screen active in minutes:");
-            DefaultParser parser =  new DefaultParser();
-            Options option = new Options();
-            option.addOption("t", "timer", true, "Set the timer for the screen to stay active");
-
+    private static void createScreen(String[] args)throws Exception {
+        // String timer = parseArguments(args,0);
+        String timer = "0";
+        if (args.length > 0){
+            log.info("Timer: " + args[0]);
+            timer = args[0];
         }
         log.info("Timer: " + timer);
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    private static void setTimer(){
-
-    }
-
-    private static String parseArguments(String[] args, int number) {
-        if (args == null || args.length == 0) {
-            throw new IllegalArgumentException("Arguments cannot be null");
+        if ("0".equals(timer)){
+            log.info("Please enter an integer in minutes to keep the screen active:");
+            System.out.println("test");
+            try (
+                Scanner in = new Scanner(System.in)){
+                int minutes = 0;
+                if (StringUtils.isNumeric(in.toString())){
+                    minutes = in.nextInt();
+                    in.nextLine();
+                }   log.info("You entered string " + minutes);
+                /*  DefaultParser parser =  new DefaultParser();
+                Options option = new Options();
+                option.addOption("t", "timer", true, "Set the timer for the screen to stay active");
+                 */
+                in.close();
+                timer = Integer.toString(minutes);
+            }
         }
-        if (args.length < number) {
-            throw new IllegalArgumentException("Invalid number of arguments");
-        }
-       return args[number];
+        log.info("Timer: " + timer);
     }
 }
